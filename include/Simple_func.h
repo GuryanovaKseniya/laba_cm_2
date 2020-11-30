d66325a815460e5d1f2e4f5bc60c6f8441fdd24b
hrenorubka <ermolaevilya@mail.ru>
2020-11-30 15:32:48 +0300

    Add function u

--- include/Simple_func.h
+++ include/Simple_func.h
@@ -3,10 +3,11 @@
 #define __Simple_func_H__
 //This file contains simple functions needed by all algorithms of the program.( Google Translate =) )
 
-
+#define _USE_MATH_DEFINES
 #include <vector>
 #include <cmath>
 #include <iostream>
+
 
 
 const double ksi = 0.4;
@@ -20,5 +21,14 @@ inline double q1(double x) { return x; }
 inline double q2(double x) { return x * x; }
 inline double f1(double x) { return x; }
 inline double f2(double x) { return exp(-x); }
-
+inline double u(double x) 
+{
+	double c1 = 0.7348403689121162;
+	double c2 = -1.7348403689121161;
+	double c3 = -0.670609108165965;
+	double c4 = -3.6275438934922133;
+	if (x > ksi)
+		return c1 * exp(sqrt(2/7) * x) + c2 * exp(-sqrt(2/7) * x) + 1;
+	else return c3 * exp(sqrt(2 / 5) * x) + c4 * exp(-sqrt(2 / 5) * x) - 6.25 * exp(-0.4);
+}
 #endif
